2c26531ceb08a4d1f2f938cf73c8fb8346fbd259
Connor <56240039+ComputerConnor@users.noreply.github.com>
2026-07-17 03:30:03 +0000

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css"
+import MeshBackground from "./components/MeshBackground";
+import Foreground from "./components/Foreground";
+
+function App() {
+  return (
+    <div className="landing-page">
+      <MeshBackground />
+      <Foreground />
+    </div>
+  );
+}
+
+export default App;
