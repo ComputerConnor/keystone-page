34865abb4e2bc268bf13b27371ca2ab4536ef5e1
Connor <56240039+ComputerConnor@users.noreply.github.com>
2026-07-28 18:52:30 -0500

    Add JPSubmitCase route to App component

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import DegenChat from "./pages/DegenChat";
 import ExploitChat from "./pages/ExploitChat";
 import JPCaseQueue from "./pages/JPCaseQueue";
 import JPSubmissionQueue from "./pages/JPSubmissionQueue";
+import JPSubmitCase from "./pages/JPSubmitCase";
 
 function App() {
 
@@ -66,6 +67,10 @@ function App() {
                         <Route
                             path="/jp/submissions"
                             element={<JPSubmissionQueue />}
+                        />
+                        <Route
+                            path="/jp/submit"
+                            element={<JPSubmitCase />}
                         />
 
                     </Routes>
