b3e56aecff03042c5c417ca12a1ab70220bf516a
Connor <56240039+ComputerConnor@users.noreply.github.com>
2026-07-29 23:58:16 +0000

    Refactor Home component structure and imports

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,29 +1,49 @@
-import { Link } from "react-router-dom";
+import {
+    Link
+} from "react-router-dom";
+
 import Navbar from "../components/Navbar";
+
 import "./Home.css";
 
-function Home(){
-    return(
+
+function Home() {
+    return (
         <div className="home-page">
+
             <Navbar />
+
             <main className="home-content">
+
                 <h1>
                     KEYSTONE
                 </h1>
+
                 <div className="home-menu">
+
                     <Link to="/cases">
                         CASES
                     </Link>
+
+                    <Link to="/submit-case">
+                        SUBMIT CASE
+                    </Link>
+
                     <Link to="/tierlist">
                         TIERLIST
                     </Link>
+
                     <Link to="/groups">
                         GROUP TIERS
                     </Link>
+
                 </div>
+
             </main>
+
         </div>
     );
 }
+
 
 export default Home;
