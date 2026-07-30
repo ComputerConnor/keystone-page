bdfe7505c183e4300f7a282c4fa6b3afcf5d8715
Connor <56240039+ComputerConnor@users.noreply.github.com>
2026-07-30 19:49:37 +0000

    Change JPLogin component to use JPSettings.css

--- src/pages/JPLogin.jsx
+++ src/pages/JPLogin.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import API_BASE from "../utils/api";
-import "./JPLogin.css";
+import "./JPSettings.css";
 
 function JPLogin() {
     const navigate = useNavigate();
